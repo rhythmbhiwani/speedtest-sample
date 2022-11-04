402201a21251a4c2574d4c9b7051c67d4f2e73be
Rhythm Bhiwani <47269183+rhythmbhiwani@users.noreply.github.com>
2022-11-04 18:02:01 +0530

    Add files via upload

---
+++ _next/static/3ptAZt5IEaeWB-KC4G-Xy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/951-fce5447e5d94abc0.js","static/chunks/pages/index-a4aa4168900ff326.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
